d9e1f890cd461018bce099d7dd21e42c31369d4f
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-03-24 15:08:26 +0530

    Add City controller with Json results

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    //[Route("api/cities")]
+    [Route("api/[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+           
+        }
+
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult (CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == id));
+        }
+    }
+}
