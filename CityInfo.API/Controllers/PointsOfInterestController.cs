86e55bbae47c3116504d5a9278563a19594345ff
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-03-31 16:54:53 +0530

    GetPointsOfInterest and GetPointOfInterst methods

---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,47 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if(city == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{pointOfIntersetId}")]
+
+        public ActionResult<PointOfInterestDto> GetPointOfInterst(int cityId, int pointOfIntersetId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            //check Point Of Interst
+
+            var pointOfInterst =  city.PointsOfInterest.FirstOrDefault(p => p.Id == pointOfIntersetId);
+
+            if (pointOfInterst == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pointOfInterst);
+        }
+    }
+}
