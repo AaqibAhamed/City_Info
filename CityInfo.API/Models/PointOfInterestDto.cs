99b7cbfc1184b7993f76b617d236a10d61fcbedf
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-03-31 11:56:30 +0530

    Add PointOfInterestDto

---
+++ CityInfo.API/Models/PointOfInterestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CityInfo.API.Models
+{
+    public class PointOfInterestDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+       
+    }
+}
