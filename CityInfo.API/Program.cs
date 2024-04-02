32d056d4483c00c6e14ebc32b3bcbe717078d8cd
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-04-02 17:04:01 +0530

    Add AddProblemDetails

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -3,6 +3,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//builder.Services.AddControllers(options =>
+//{
+//    options.ReturnHttpNotAcceptable = true;
+//});
+
+builder.Services.AddProblemDetails(options =>
+{
+    options.CustomizeProblemDetails = ctx =>
+    {
+        ctx.ProblemDetails.Extensions.Add("additionalInfo", "Additional info example");
+        ctx.ProblemDetails.Extensions.Add("server", Environment.MachineName);
+    };
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
