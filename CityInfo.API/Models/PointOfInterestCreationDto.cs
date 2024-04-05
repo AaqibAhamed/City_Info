224b7d6ba041bf1cd5df5b3083bc22afb9cfa804
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-04-05 18:08:12 +0530

    Add validation with DataAnnotations

--- CityInfo.API/Models/PointOfInterestCreationDto.cs
+++ CityInfo.API/Models/PointOfInterestCreationDto.cs
@@ -1,9 +1,14 @@
-﻿namespace CityInfo.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
 {
     public class PointOfInterestCreationDto
     {
+        [Required(ErrorMessage ="Please provide a name value")]
+        [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
 
+        [MaxLength(200)]
         public string? Description { get; set; }
     }
 }
