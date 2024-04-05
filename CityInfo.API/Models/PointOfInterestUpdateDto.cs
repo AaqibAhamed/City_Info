7476adde2bd8132336d4ea690129185bb7c201a8
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-04-05 20:48:03 +0530

    Add PointOfInterestUpdateDto

---
+++ CityInfo.API/Models/PointOfInterestUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
+{
+    public class PointOfInterestUpdateDto
+    {
+        [Required(ErrorMessage = "Please provide a name value")]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+    }
+}
