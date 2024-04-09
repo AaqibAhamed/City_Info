06d374fd0ea3d090a315bd7715f939611d0a7413
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-04-09 12:09:58 +0530

    Add ILogger

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -8,6 +8,12 @@ namespace CityInfo.API.Controllers
     [ApiController]
     public class PointsOfInterestController : Controller
     {
+        private readonly ILogger<PointsOfInterestController> _logger;
+
+        public PointsOfInterestController(ILogger<PointsOfInterestController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger)) ;
+        }
 
         [HttpGet]
         public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
@@ -16,6 +22,7 @@ namespace CityInfo.API.Controllers
 
             if (city == null)
             {
+                _logger.LogInformation($"City with id {cityId} was mot found when accessing PointsOfInterest ");
                 return NotFound();
             }
 
