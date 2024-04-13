75689411e8a3d3b7288ab55a08ba12f0d4d175fc
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-04-13 17:53:45 +0530

    Register LocalMailService and CloudMailService

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,17 +1,19 @@
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.StaticFiles;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.Console()
-    .WriteTo.File("logs/cityinfo.txt",rollingInterval: RollingInterval.Day)
+    .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
-/*builder.Logging.ClearProviders(); // Inbuild logger no need using serilog
-builder.Logging.AddConsole();*/ // Inbuild logger no need using serilog
-builder.Host.UseSerilog();
+//builder.Logging.ClearProviders(); // Inbuild logger no need using serilog
+//builder.Logging.AddConsole(); // Inbuild logger no need using serilog
+
+builder.Host.UseSerilog(); //register serilog
 
 // Add services to the container.
 
@@ -39,6 +41,12 @@ builder.Services.AddProblemDetails();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<FileExtensionContentTypeProvider>(); // mapping file extentions in requestion application/pdf
+
+# if DEBUG
+builder.Services.AddTransient<IMailService, LocalMailService>();
+# else
+builder.Services.AddTransient<IMailService, CloudMailService>();
+# endif
 
 var app = builder.Build();
 
