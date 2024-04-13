ef69c537ee1979b12ec0541e8521885924384cc0
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-04-13 17:52:25 +0530

    Ad LocalMailService and IMailService

---
+++ CityInfo.API/Services/LocalMailService.cs
@@ -0,0 +1,17 @@
+﻿namespace CityInfo.API.Services
+{
+    public class LocalMailService : IMailService
+    {
+        private string mailTo = "admin@cityinfo.com";
+
+        private string mailFrom = "noreply@cityinfo.com";
+
+        public void Send(string subject, string message)
+        {
+            Console.WriteLine($"Mail from {mailFrom} mail to {mailTo}, with {nameof(LocalMailService)}. ");
+            Console.WriteLine($"Subjet : {subject}");
+            Console.WriteLine($"Mail: {message}");
+
+        }
+    }
+}
