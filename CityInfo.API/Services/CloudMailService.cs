127878d6f3e11fd07e8346c548fb995e54a0a7f4
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-04-13 17:51:36 +0530

    Ad CloudMailService

---
+++ CityInfo.API/Services/CloudMailService.cs
@@ -0,0 +1,17 @@
+﻿namespace CityInfo.API.Services
+{
+    public class CloudMailService : IMailService
+    {
+        private string mailTo = "admin@cityinfo.com";
+
+        private string mailFrom = "noreply@cityinfo.com";
+
+        public void Send(string subject, string message)
+        {
+            Console.WriteLine($"Mail from {mailFrom} mail to {mailTo}, with {nameof(CloudMailService)}. ");
+            Console.WriteLine($"Subjet : {subject}");
+            Console.WriteLine($"Mail: {message}");
+
+        }
+    }
+}
