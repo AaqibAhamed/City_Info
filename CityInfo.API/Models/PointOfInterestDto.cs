a62d87e965f24ae1840013b20f3805f2d77fecc8
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-04-14 20:57:40 +0530

    Add PointOfInterest Enitity

--- CityInfo.API/Models/PointOfInterestDto.cs
+++ CityInfo.API/Models/PointOfInterestDto.cs
@@ -6,7 +6,7 @@ namespace CityInfo.API.Models
     {
         public int Id { get; set; }
 
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;  
 
         public string? Description { get; set; }
 
