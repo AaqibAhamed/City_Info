beb39c938b81c4abe41e8a3a4a17095d06873dad
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-04-14 20:57:30 +0530

    Add City Enitity

---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,28 @@
+﻿using CityInfo.API.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+    public class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } // = string.Empty; constructor added 
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+            = new List<PointOfInterestDto>();
+
+        public City(string name)
+        {
+            Name = name;
+        }
+    }
+}
