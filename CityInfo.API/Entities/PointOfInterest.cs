4f71d63846832cee328f3ef8543f4646cb26d950
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-04-15 11:18:20 +0530

    Add Description

--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -13,7 +13,7 @@ namespace CityInfo.API.Entities
         [MaxLength(50)]
         public string Name { get; set; }
 
-        //  public string? Description { get; set; }
+        public string? Description { get; set; }
 
         [ForeignKey("CityId")]
         public City? City { get; set; }
