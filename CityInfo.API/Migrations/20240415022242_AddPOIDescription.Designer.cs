35998b778662326f1c1043082c99e6e85fa4a45e
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-04-15 11:18:07 +0530

    Migrations

--- CityInfo.API/Migrations/20240415022242_AddPOIDescription.Designer.cs
+++ CityInfo.API/Migrations/20240415022242_AddPOIDescription.Designer.cs
@@ -2,6 +2,7 @@
 using CityInfo.API.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CityInfo.API.Migrations
 {
     [DbContext(typeof(CityInfoContext))]
-    partial class CityInfoContextModelSnapshot : ModelSnapshot
+    [Migration("20240415022242_AddPOIDescription")]
+    partial class AddPOIDescription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
