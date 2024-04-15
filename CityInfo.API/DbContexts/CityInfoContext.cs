ef47770d47e572e8b8db8bcf3cc24b9a2aa46702
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-04-15 07:39:55 +0530

    Add CityInfoContext

---
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -0,0 +1,23 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.DbContexts
+{
+    public class CityInfoContext : DbContext
+    {
+        public CityInfoContext(DbContextOptions<CityInfoContext> options): base(options) { }
+
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<PointOfInterest> PointOfInterests { get; set; }
+
+       /* protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("connectionstring");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+*/
+
+    }
+}
