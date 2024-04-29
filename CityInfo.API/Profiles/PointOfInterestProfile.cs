d51627adcfd595d20dfe8da296df7749f2703cd0
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-04-29 10:47:25 +0530

    Add PointOfInterestProfilefor Auto Mapper

---
+++ CityInfo.API/Profiles/PointOfInterestProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CityInfo.API.Entities;
+using CityInfo.API.Models;
+
+namespace CityInfo.API.Profiles
+{
+    public class PointOfInterestProfile : Profile
+    {
+        public PointOfInterestProfile() 
+        {
+            CreateMap<PointOfInterest, PointOfInterestDto>(); // Get -Entity to Dto
+
+            CreateMap<PointOfInterestCreationDto, PointOfInterest>(); // POST
+
+            CreateMap<PointOfInterestUpdateDto, PointOfInterest>(); //PUT 
+
+            CreateMap<PointOfInterest,PointOfInterestUpdateDto>(); // Fetch
+        }
+    }
+}
