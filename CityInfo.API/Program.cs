a75aa282516ebf3b8e6a1d31561efd47203c842f
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-04-29 10:48:40 +0530

    Install and  AutoMapper

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -59,6 +59,8 @@ builder.Services.AddDbContext<CityInfoContext>(DbContextOptions  // Regsiter wit
 
 builder.Services.AddScoped<ICityInfoRepository, CityInfoRepository>();
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
