28ca4bfc33389a95b77c177da2845f05100cb1eb
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-05-07 21:47:36 +0530

    Add GetCitiesByFiterAsync

--- CityInfo.API/Services/ICityInfoRepository.cs
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -6,6 +6,8 @@ namespace CityInfo.API.Services
     {
         Task<IEnumerable<City>> GetAllCitiesAsync();
 
+        Task<IEnumerable<City>> GetCitiesByFiterAsync(string? name);
+
         Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest);
 
         Task<bool> CityExistsAsync(int cityId);
