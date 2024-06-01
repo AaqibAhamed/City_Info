2990c77d7ae89301aee78ed4bab0159bd448e9ec
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-06-01 17:12:20 +0530

    Add Model summary

--- CityInfo.API/Models/CityWithoutPointOfInterestDto.cs
+++ CityInfo.API/Models/CityWithoutPointOfInterestDto.cs
@@ -1,11 +1,23 @@
 ﻿namespace CityInfo.API.Models
 {
+    /// <summary>
+    /// A city without points of interest
+    /// </summary>
     public class CityWithoutPointOfInterestDto
     {
+        /// <summary>
+        /// The id of the city
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// The name of the city 
+        /// </summary>
         public string Name { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The description of the city
+        /// </summary>
         public string? Description { get; set; }
     }
 }
